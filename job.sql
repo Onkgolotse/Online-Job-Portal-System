b2a484c73c272971076d602668c79b77a70dd83c
Onkgolotse <81123940+Onkgolotse@users.noreply.github.com>
2022-06-01 13:10:25 +0200

    Initial commit

---
+++ job.sql
@@ -0,0 +1,379 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2022 at 02:40 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `job`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application_master`
+--
+
+CREATE TABLE `application_master` (
+  `ApplicationId` int(11) NOT NULL,
+  `JobSeekId` int(11) NOT NULL,
+  `JobId` int(11) NOT NULL,
+  `Status` varchar(30) NOT NULL,
+  `Description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `application_master`
+--
+
+INSERT INTO `application_master` (`ApplicationId`, `JobSeekId`, `JobId`, `Status`, `Description`) VALUES
+(1, 3, 1, 'Call Latter Send', 'Invited on 21-Dec-2013.'),
+(2, 3, 2, 'Call Latter Send', 'You are Invited For Interview on 10-MAR-2013.'),
+(3, 3, 3, 'Call Latter Send', 'Invited on 21-Dec-2013.'),
+(5, 3, 4, 'Call Latter Send', 'Invited on 21-Dec-2013.'),
+(6, 7, 5, 'Apply', 'No Message'),
+(7, 7, 6, 'Apply', 'No Message');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employer_reg`
+--
+
+CREATE TABLE `employer_reg` (
+  `EmployerId` int(11) NOT NULL,
+  `CompanyName` varchar(20) NOT NULL,
+  `ContactPerson` varchar(20) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `City` varchar(20) NOT NULL,
+  `Email` varchar(40) NOT NULL,
+  `Mobile` bigint(20) NOT NULL,
+  `Area_Work` varchar(40) NOT NULL,
+  `Status` varchar(10) NOT NULL,
+  `UserName` varchar(20) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Question` varchar(100) NOT NULL,
+  `Answer` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employer_reg`
+--
+
+INSERT INTO `employer_reg` (`EmployerId`, `CompanyName`, `ContactPerson`, `Address`, `City`, `Email`, `Mobile`, `Area_Work`, `Status`, `UserName`, `Password`, `Question`, `Answer`) VALUES
+(2, 'TCS Private Limited', 'Mr. Mohan Shah', 'Navarangpura1', 'Ahmedabad', 'mohan@gmail.com', 9898989898, 'Software', 'Confirm', 'mohan', 'mohan', 'Who is Your Favourite Person?', 'sachin'),
+(3, 'Wipro Infotech', 'Mr. Sunil Pandya', 'Baroda', 'Baroda', 'sunil@wipro.com', 8989898989, 'Hardware', 'Confirm', 'sunil', 'sunil', '', ''),
+(4, 'Solusoft  Pvt Limite', 'Mr. Nirav Soni', 'Narayanpura', 'Ahmedabad', 'nirav@gmail.com', 9898989898, 'Software', 'Confirm', 'nirav', 'nirav', 'What is Your Pet Name?', 'niru'),
+(5, 'Info Matrics', 'Mr. Narayan', 'Sahibagh', 'Ahmedabad', 'narayan@yahoo.com', 6767676767, 'Software', 'Confirm', 'narayan', 'narayan', 'What is Your Pet Name?', 'nari'),
+(6, 'Aquiver Holdings PTY', 'Etsile', 'P O Box 151, Otse, Botswana', 'Gaborone', 'etsileedwin@gmail.com', 26771792961, 'Computer science', 'Confirm', 'etsilekgosana@gmail.', 'diutlwileng', 'What is the Name of Your First School?', 'Little souls');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `FeedbackId` int(11) NOT NULL,
+  `JobSeekId` int(11) NOT NULL,
+  `Feedback` varchar(200) NOT NULL,
+  `FeedbakDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`FeedbackId`, `JobSeekId`, `Feedback`, `FeedbakDate`) VALUES
+(5, 3, 'asdad', '2018-09-13'),
+(6, 3, 'asd', '2013-09-18'),
+(7, 4, 'Thanks For Your Support.', '2013-09-18'),
+(8, 3, 'asd', '2013-09-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobseeker_education`
+--
+
+CREATE TABLE `jobseeker_education` (
+  `EduId` int(11) NOT NULL,
+  `JobSeekId` int(11) NOT NULL,
+  `Degree` varchar(20) NOT NULL,
+  `University` varchar(100) NOT NULL,
+  `PassingYear` mediumint(9) NOT NULL,
+  `Percentage` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jobseeker_education`
+--
+
+INSERT INTO `jobseeker_education` (`EduId`, `JobSeekId`, `Degree`, `University`, `PassingYear`, `Percentage`) VALUES
+(3, 3, 'B.C.A', 'Ganpat Universiy', 2011, 68.89),
+(4, 3, 'M.C.A', 'Ganpat University', 2013, 89.9),
+(5, 3, 'S.S.C', 'GSEB', 2005, 80);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobseeker_reg`
+--
+
+CREATE TABLE `jobseeker_reg` (
+  `JobSeekId` int(11) NOT NULL,
+  `JobSeekerName` varchar(20) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `City` varchar(20) NOT NULL,
+  `Email` varchar(40) NOT NULL,
+  `Mobile` bigint(20) NOT NULL,
+  `Qualification` varchar(20) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `BirthDate` date NOT NULL,
+  `Resume` varchar(200) NOT NULL,
+  `Status` varchar(10) NOT NULL,
+  `UserName` varchar(20) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Question` varchar(100) NOT NULL,
+  `Answer` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jobseeker_reg`
+--
+
+INSERT INTO `jobseeker_reg` (`JobSeekId`, `JobSeekerName`, `Address`, `City`, `Email`, `Mobile`, `Qualification`, `Gender`, `BirthDate`, `Resume`, `Status`, `UserName`, `Password`, `Question`, `Answer`) VALUES
+(3, 'Jalpa Prajapati', 'Near Dena Bank', 'Mehsana', 'jalpa@gmail.com', 9898989898, 'M.C.A', 'Female', '2013-09-10', 'Marksheet.pdf', 'Confirm', 'jalpa', 'jalpa', 'What is Your Pet Name?', 'jalpa'),
+(4, 'Krunal Prajapati', 'Patan', 'Patan', 'krunal@gmail.com', 8989898989, 'M.B.A', 'Male', '2013-09-16', 'Marksheet.pdf', 'Confirm', 'krunal', 'krunal', '', ''),
+(5, 'Gopal Patel', 'Patan', 'Patan', 'gopal@gmail.com', 9898989898, 'MA', 'Male', '2013-10-15', 'admin.jpg', 'Confirm', 'gopal', 'gopal', '', ''),
+(6, 'Mehul Mistry', 'Swastik SOciety', 'Baroda', 'mehul@gmail.com', 8989898998, 'BE', 'Male', '2013-10-09', '470X310_1.jpg', 'Confirm', 'mehul', 'mehul', 'What is Your Pet Name?', 'mehu'),
+(7, 'Etsile Kgosana', 'P o box 151, otse', 'Gaborone', 'etsilekgosana@gmail.com', 71792961, 'Computer system engi', 'Male', '2002-05-22', 'Etsile_Kgosana_-_ (3).docx', 'Confirm', 'etsilekgosana@gmail.', 'diutlwileng', 'What is the Name of Your First School?', 'Love to learn'),
+(8, 'Pako Tshepiso', 'P o box 10, ftown', 'francistown', 'pako@gmail.com', 71234567, '', 'Male', '2020-06-02', 'FB316110-67C0-4A3C-80BE-C999AEF244B2.jpeg', 'Pending', 'admin', 'admin', 'Who is Your Favourite Person?', 'mr tom');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_master`
+--
+
+CREATE TABLE `job_master` (
+  `JobId` int(11) NOT NULL,
+  `CompanyName` varchar(20) NOT NULL,
+  `JobTitle` varchar(50) NOT NULL,
+  `Vacancy` int(11) NOT NULL,
+  `MinQualification` varchar(50) NOT NULL,
+  `Description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `job_master`
+--
+
+INSERT INTO `job_master` (`JobId`, `CompanyName`, `JobTitle`, `Vacancy`, `MinQualification`, `Description`) VALUES
+(1, 'Wipro Infotech', 'Software Professional Required', 2, 'M.C.A', 'ASP.NET'),
+(2, 'Wipro Infotech', 'Marketing Executive Required', 5, 'M.B.A', 'Freshers Are Invited'),
+(3, 'TCS Private Limited', 'Software Trainee Required', 1, 'B.Sc.I.T', 'Starting Salary 5000'),
+(4, 'Wipro Infotech', 'Cleaners Required', 3, 'S.S.C', 'N'),
+(5, 'Aquiver Holdings PTY', 'Security guard', 10, 'None', 'Work in north-east primary schools'),
+(6, 'Aquiver Holdings PTY', 'Cleaning Ladies', 20, 'None', 'We are looking for young enegetic cliners'),
+(7, 'Aquiver Holdings PTY', 'Accountant', 2, 'Ba Acoounting', 'Looking for a young energetic motivated individual.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_master`
+--
+
+CREATE TABLE `news_master` (
+  `NewsId` int(11) NOT NULL,
+  `News` varchar(200) NOT NULL,
+  `NewsDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `news_master`
+--
+
+INSERT INTO `news_master` (`NewsId`, `News`, `NewsDate`) VALUES
+(1, 'Register and Get JOB', '2013-09-24'),
+(2, 'New Vacancies will be updated after diwali', '2013-10-31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_master`
+--
+
+CREATE TABLE `user_master` (
+  `UserId` int(11) NOT NULL,
+  `UserName` varchar(20) NOT NULL,
+  `Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_master`
+--
+
+INSERT INTO `user_master` (`UserId`, `UserName`, `Password`) VALUES
+(6, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `walkin_master`
+--
+
+CREATE TABLE `walkin_master` (
+  `WalkInId` int(11) NOT NULL,
+  `CompanyName` varchar(20) NOT NULL,
+  `JobTitle` varchar(50) NOT NULL,
+  `Vacancy` int(11) NOT NULL,
+  `MinQualification` varchar(50) NOT NULL,
+  `Description` varchar(100) NOT NULL,
+  `InterviewDate` date NOT NULL,
+  `InterviewTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `walkin_master`
+--
+
+INSERT INTO `walkin_master` (`WalkInId`, `CompanyName`, `JobTitle`, `Vacancy`, `MinQualification`, `Description`, `InterviewDate`, `InterviewTime`) VALUES
+(1, 'Wipro Infotech', 'Freshers Required', 5, 'B.C.A', 'Hardworking Person are Required.', '2013-09-25', '09:00:00'),
+(2, 'TCS Private Limited', 'Marketive Representative Invited', 2, 'Pharmacist', 'Ready TO work in North Gujarat', '2013-10-07', '09:00:00'),
+(3, 'Aquiver Holdings PTY', 'Cleaners', 20, 'None', 'Highy motivated', '2021-06-02', '00:14:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `application_master`
+--
+ALTER TABLE `application_master`
+  ADD PRIMARY KEY (`ApplicationId`);
+
+--
+-- Indexes for table `employer_reg`
+--
+ALTER TABLE `employer_reg`
+  ADD PRIMARY KEY (`EmployerId`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`FeedbackId`);
+
+--
+-- Indexes for table `jobseeker_education`
+--
+ALTER TABLE `jobseeker_education`
+  ADD PRIMARY KEY (`EduId`);
+
+--
+-- Indexes for table `jobseeker_reg`
+--
+ALTER TABLE `jobseeker_reg`
+  ADD PRIMARY KEY (`JobSeekId`),
+  ADD KEY `JobSeekId` (`JobSeekId`);
+
+--
+-- Indexes for table `job_master`
+--
+ALTER TABLE `job_master`
+  ADD PRIMARY KEY (`JobId`);
+
+--
+-- Indexes for table `news_master`
+--
+ALTER TABLE `news_master`
+  ADD PRIMARY KEY (`NewsId`);
+
+--
+-- Indexes for table `user_master`
+--
+ALTER TABLE `user_master`
+  ADD PRIMARY KEY (`UserId`);
+
+--
+-- Indexes for table `walkin_master`
+--
+ALTER TABLE `walkin_master`
+  ADD PRIMARY KEY (`WalkInId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `application_master`
+--
+ALTER TABLE `application_master`
+  MODIFY `ApplicationId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `employer_reg`
+--
+ALTER TABLE `employer_reg`
+  MODIFY `EmployerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `FeedbackId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `jobseeker_education`
+--
+ALTER TABLE `jobseeker_education`
+  MODIFY `EduId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `jobseeker_reg`
+--
+ALTER TABLE `jobseeker_reg`
+  MODIFY `JobSeekId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `job_master`
+--
+ALTER TABLE `job_master`
+  MODIFY `JobId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `news_master`
+--
+ALTER TABLE `news_master`
+  MODIFY `NewsId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_master`
+--
+ALTER TABLE `user_master`
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `walkin_master`
+--
+ALTER TABLE `walkin_master`
+  MODIFY `WalkInId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
